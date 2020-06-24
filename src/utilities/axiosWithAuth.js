0fb4f2337f28b95d73c0d3669ff8688b6de89267
Chris <chrisdel@protonmail.com>
2020-06-24 11:47:17 -0400

    fixed the authwithaxios ult

--- src/utilities/axiosWithAuth.js
+++ src/utilities/axiosWithAuth.js
@@ -1,18 +1,20 @@
-import axios from "axios";
-require("dotenv").config();
+import axios from 'axios';
+require('dotenv').config();
 
 const axiosWithAuth = () => {
-  const token = localStorage.getItem("token");
+  const token = localStorage.getItem('token');
   // retrieve API base url from .env
-  const apiUrl = "http://localhost:9000";
+  const apiUrl =
+    process.env.REACT_APP_URL || 'https://mudadventurebackend.herokuapp.com';
+
   console.log(apiUrl);
   return axios.create({
     // sets base url, and authorization headers with each axios request
     baseURL: apiUrl,
     headers: {
-      "Content-Type": "application/json",
-      Authorization: token
-    }
+      'Content-Type': 'application/json',
+      Authorization: token,
+    },
   });
 };
 
